232d00899f63175ba650adce6e28eda774172080
youssef-md <emaildeyoussefmuhamad@gmail.com>
2019-09-16 21:42:32 -0300

    Sanitize, format and save the question input to serve the c++ algorithm

--- server/app.js
+++ server/app.js
@@ -1,13 +1,30 @@
+const fs = require('fs')
 const express = require('express')
 const server = express()
 
 server.use(express.json())
 
+const sanitizeArrToString = (str) => JSON.stringify(str).split(/[\[\],]/).join(' ').trim()
+
 server.post('/graph', (req, res) => {
-  const { qtdElevator, selectedFloor, elevatorsTimes, elevatorsPaths } = req.body
-  console.log(qtdElevator, selectedFloor, elevatorsTimes, elevatorsPaths)
+  let { qtdElevator, selectedFloor, elevatorsTimes, elevatorsPaths } = req.body
+  // console.log(qtdElevator, selectedFloor, elevatorsTimes, elevatorsPaths)
 
+  elevatorsTimes = sanitizeArrToString(elevatorsTimes)
 
+  strPaths = ''
+  elevatorsPaths.map((path, i) => {
+    path = sanitizeArrToString(path)
+    strPaths += `${path}${i + 1 !== elevatorsPaths.length ? '\n' : ''}`
+  })
+
+  const questionInput = `${qtdElevator} ${selectedFloor}\n${elevatorsTimes}\n${strPaths}\n`
+
+  fs.writeFile('./solver/input.txt', questionInput, err => {
+    if (err) throw new Error('Lascou!')
+
+    console.log('saved')
+  })
 
   res.send({ ok: true })
 })
